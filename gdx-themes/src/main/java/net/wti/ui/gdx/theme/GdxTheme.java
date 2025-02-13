f0a3c3d86852cc8207f9b0aaae6c09abba4feb15
James X. Nelson <James@WeTheInter.net>
2025-02-13 01:46:55 -0600

    Add raeleus themes / resources

---
+++ gdx-themes/src/main/java/net/wti/ui/gdx/theme/GdxTheme.java
@@ -0,0 +1,13 @@
+package net.wti.ui.gdx.theme;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+/// GdxTheme:
+///
+///
+/// Created by James X. Nelson (James@WeTheInter.net) on 13/02/2025 @ 00:56
+public interface GdxTheme {
+
+    Skin getSkin();
+
+}
